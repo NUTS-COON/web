adf451171cf643f68ee8926c541f2fdbdf7e5e7e
NUTS-COON <mburavczov@gmail.com>
2017-10-25 16:55:07 +0300

    gunicorn

---
+++ hello.py
@@ -0,0 +1,14 @@
+def app(environ, start_response):
+
+    string = environ["QUERY_STRING"].split('&')
+    data = []
+    for i in string:
+        data.append(i + '\n')    
+     
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return data
